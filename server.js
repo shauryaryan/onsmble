d83ddb46de8197277faac9be3bb2080a4e66af6c
shauryaryan <sajb1528@gmail.com>
2024-11-12 14:41:31 -0600

    Add server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
